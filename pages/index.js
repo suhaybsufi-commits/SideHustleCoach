b878b1fcabb8804ecd75624b4a7dbf997676de66
suhaybsufi-commits <suhaybsufi@gmail.com>
2025-09-03 14:08:45 +0100

    Add homepage
    Add homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to SideHustleCoach 🚀</h1>
+      <p>Your AI-powered side hustle guide is coming soon.</p>
+    </div>
+  );
+}
