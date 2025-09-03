e18c1b8cbb3afa9b0f42d724046a354b04f7e4fd
suhaybsufi-commits <suhaybsufi@gmail.com>
2025-09-03 14:43:26 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <nav>
-        <Link href="/">Home</Link> |{" "}
-        <Link href="/about">About</Link> |{" "}
-        <Link href="/blog">Blog</Link> |{" "}
-        <Link href="/contact">Contact</Link>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/blog">Blog</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
+        </ul>
       </nav>
 
       <h1>Welcome to SideHustleCoach 🚀</h1>
