d66477257633cbaaee026c13cea6c3bb6e1ac454
suhaybsufi-commits <suhaybsufi@gmail.com>
2025-09-03 14:45:16 +0100

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About SideHustleCoach</h1>
+      <p>This platform helps you discover and grow AI-powered side hustles step by step.</p>
+    </div>
+  );
+}
